30edd578aa2ae57c647ffe51fbaedc7e9db8e503
gjlee0802 <gjlee0802@naver.com>
2021-05-21 14:21:18 +0900

    decorator.py

--- pytet_v0.7/decorator.py
+++ pytet_v0.7/decorator.py
@@ -11,8 +11,6 @@ class ColorDecorator(Game):
 	setOfCBlockObjects = 0
 	def initCBlocks(self, setOfBlockObjects):
 		### initialize self.setOfCBlockObjects
-		#Tetris.nBlockTypes = len(setOfBlockArrays)
-		#Tetris.nBlockDegrees = len(setOfBlockArrays[0])
 		self.setOfCBlockObjects = [[0] * Tetris.nBlockDegrees for _ in range(Tetris.nBlockTypes)]
 		arrayBlk_maxSize = 0
 		for i in range(Tetris.nBlockTypes):
@@ -41,11 +39,11 @@ class ColorDecorator(Game):
 			self.iCScreen = Matrix(self.oCScreen)
 		state = self.game.accept(key)	
 
-		currCBlk = self.setOfCBlockObjects[self.game.idxBlockType][self.game.idxBlockDegree]
+		self.currCBlk = self.setOfCBlockObjects[self.game.idxBlockType][self.game.idxBlockDegree]
 		tempBlk = self.iCScreen.clip(self.game.top, self.game.left,
-					self.game.top + currCBlk.get_dy(),
-					self.game.left + currCBlk.get_dx())
-		tempBlk = tempBlk + currCBlk
+					self.game.top + self.currCBlk.get_dy(),
+					self.game.left + self.currCBlk.get_dx())
+		tempBlk = tempBlk + self.currCBlk
 		self.oCScreen = Matrix(self.iCScreen)
 		self.oCScreen.paste(tempBlk, self.game.top, self.game.left)
 		return state
@@ -54,22 +52,18 @@ class ColorDecorator(Game):
 		return self.oCScreen
 	
 	def deleteFullLines(self):
-		self.game.deleteFullLines()
-		nDeleted = 0
-		nScanned = self.game.currBlk.get_dy()
+                nDeleted = 0
+                nScanned = self.game.deleteFullLines()
 
-		if self.game.top + self.game.currBlk.get_dy() - 1 >= self.game.iScreenDy:
-		    nScanned -= (self.game.top + self.game.currBlk.get_dy() - self.game.iScreenDy)
-
-		zero = Matrix([[ 0 for x in range(0, (self.game.iScreenDx - 2*Tetris.iScreenDw))]])
-		for y in range(nScanned - 1, -1, -1):
-		    cy = self.game.top + y + nDeleted
-		    line = self.oCScreen.clip(cy, 0, cy+1, self.oCScreen.get_dx())
-		    if line.sum() == self.oCScreen.get_dx():
-		        temp = self.oCScreen.clip(0, 0, cy, self.oCScreen.get_dx())
-		        self.oCScreen.paste(temp, 1, 0)
-		        self.oCScreen.paste(zero, 0, self.iCScreenDw)
-		        nDeleted += 1
-
-		return
+                zero = Matrix([[ 0 for x in range(0, (self.game.iScreenDx - 2*self.game.iScreenDw))]])
+                for y in range(nScanned - 1, -1, -1):
+                    cy = self.game.top + y + nDeleted
+                    line = self.oCScreen.binary().clip(cy, 0, cy+1, self.oCScreen.get_dx())
+                    if line.sum() == self.game.oScreen.get_dx():
+                        temp = self.oCScreen.clip(0, 0, cy, self.oCScreen.get_dx())
+                        self.oCScreen.paste(temp, 1, 0)
+                        self.oCScreen.paste(zero, 0, self.game.iScreenDw)
+                        nDeleted += 1
+                
+                return
 
